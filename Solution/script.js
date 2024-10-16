ea680a7227660b07668b668c775a0d9a56fcff1b
Sebastian Hernandez <sebastianhernandezpatio.2004@gmail.com>
2024-10-16 09:11:39 -0500

    Solution of the third point

--- Solution/script.js
+++ Solution/script.js
@@ -6,4 +6,8 @@ function celsiusAFahrenheit(celsius){
     return (celsius * 9/5) + 32
 }
 
-module.exports = { esBisiesto, celsiusAFahrenheit };
+function mayorDeDos(n1, n2){
+    return n1 > n2 ? n1 : n2;
+}
+
+module.exports = { esBisiesto, celsiusAFahrenheit, mayorDeDos };
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const { esBisiesto, celsiusAFahrenheit } = require("./Solution/script");
+const { esBisiesto, celsiusAFahrenheit, mayorDeDos } = require("./Solution/script");
 
 console.log(esBisiesto(2020));
 console.log(celsiusAFahrenheit(0));
+console.log(mayorDeDos(5, 10));
